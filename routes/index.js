2b0fb93a4167db35fdea3c4870f398cf51846fa1
Daniel Ge <dange@seas.upenn.edu>
2013-04-21 13:13:00 -0400

    Add simple error handling

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,26 @@ var http = require('http');
  */
 
 exports.current = function(req, res) {
-  http.get("http://api.open-notify.org/iss-now/", function (api_res) {
-      res.setHeader("Content-Type", "application/json");
-    if (api_res.statusCode !== 200) {
-      res.statusCode = api_res.statusCode;
-      res.send(JSON.stringify({
-        "message": "Something wrong occurred!"
-      }));
+  try {
+    http.get("http://api.open-notify.org/iss-now/", function (api_res) {
+        res.setHeader("Content-Type", "application/json");
+      if (api_res.statusCode !== 200) {
+        res.statusCode = api_res.statusCode;
+        res.send(JSON.stringify({
+          "message": "Something wrong occurred!"
+        }));
 
-      return;
-    }
+        return;
+      }
 
-    api_res.on('data', function (data) {
-      res.send(data);
+      api_res.on('data', function (data) {
+        res.send(data);
+      });
     });
-  });
+  } catch (e) {
+    console.log(e);
+    res.send(500, JSON.stringify({
+      "message": "Could not GET external server"
+    }));
+  }
 }
